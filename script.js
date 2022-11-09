65ab688abf4202933f4a45a1cc84513e9c3bc1b6
Nestor <nestoruribe92@hotmail.com>
2022-11-08 20:00:34 -0500

    disable and enable button

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 let tasks = []
+document.addEventListener('DOMContentLoaded', function (){ 
+        document.querySelector('button').disabled = true
+        document.querySelector('input').onkeyup = () => {
+            document.querySelector('button').disabled = false
+        }
 
+})
 function addNewTask(){
     //tomar la informacion que esta en #input y agregarla a la variable tasks
+    
     const text = document.querySelector('#input').value;
     const li = document.createElement('li');
     li.innerHTML = text;
