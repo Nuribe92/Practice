eef0131dd8fbab835690840371e2f7e3fe9977cf
Nuribe92 <nestoruribe92@hotmail.com>
2022-11-12 15:52:20 -0500

    eliminated duplicates

--- script.js
+++ script.js
@@ -1,55 +1,53 @@
-
-document.addEventListener('DOMContentLoaded', function (){ 
-        document.querySelector('button').disabled = true
-        document.querySelector('input').onkeyup = () => {
-            if(document.querySelector('input').value.length > 0){
-                document.querySelector('button').disabled = false;  
-            }else{
-               document.querySelector('button').disabled = true; 
-            }
-        }
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelector("button").disabled = true;
+  document.querySelector("input").onkeyup = () => {
+    if (document.querySelector("input").value.length > 0) {
+      document.querySelector("button").disabled = false;
+    } else {
+      document.querySelector("button").disabled = true;
     }
-    )
-let tasks = []
+  };
+});
+let tasks = [];
+console.log(tasks);
 
-console.log(tasks)
+const division = document.getElementById("tasks");
 
-const division = document.getElementById('tasks')
+function addNewTask() {
+  //tomar la informacion que esta en #input y agregarla a la variable tasks
+  const text = document.querySelector("#input").value;
+  document.querySelector("input").value = "";
+  document.querySelector("button").disabled = true;
+  tasks.push(text);
 
-function addNewTask(){
-    //tomar la informacion que esta en #input y agregarla a la variable tasks
-    const text = document.querySelector('#input').value;
-    const li = document.createElement('li');
-    li.innerHTML = text;
-    //document.querySelector('#newTasks').append(li);
-    document.querySelector('input').value = '';
-    document.querySelector('button').disabled = true;
-    tasks.push(text) 
-    let existe = tasks[tasks.length -1];
-    tasks.forEach((tarea) => {  
-        if(tarea == existe) {    
-            const div = document.createElement('div')
-            div.classList.add('tarea');
-            div.innerHTML = `
+
+  let existe = tasks[tasks.length - 1];
+
+  tasks.forEach((tarea) => {
+    if (tarea === existe) {
+      const div = document.createElement("div");
+      div.classList.add("tarea");
+      div.innerHTML = `
             <h2><li>${tarea}</li></h2>
             <button id="internalButton">x</button>
-            `        
-            division.appendChild(div) 
-        }           
-        })
-        let close = document.querySelectorAll('#internalButton');
-            for(let i = 0; i < close.length; i++){
-                close[i].addEventListener('click', () => {
-                    close[i].parentElement.style.opacity = 0;
-                    setTimeout(()=> {    
-                close[i].parentElement.style.display = "none";
-                close[i].parentElement.remove();
-            }, 500); 
-    })
+            `;
+      division.appendChild(div);
+    }
+  });
+  let close = document.querySelectorAll("#internalButton");
+  for (let i = 0; i < close.length; i++) {
+    close[i].addEventListener("click", () => {
+      close[i].parentElement.style.opacity = 0;
+      setTimeout(() => {
+        close[i].parentElement.style.display = "none";
+        close[i].parentElement.remove();
+        eliminarTarea();
+      }, 500);
+    });
+  }
 }
-}
-
-
-
-
-
+const eliminarTarea = (tareita) => {
+  const item = tasks.find((tarea) => tarea === tareita);
+  let indice = tasks.indexOf(item);
+  tasks.splice(indice, 1);
+};
