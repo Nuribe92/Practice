f71f80e751f3a23892c3eaa04a7a85cabd59aab6
Nuribe92 <nestoruribe92@hotmail.com>
2022-11-17 10:25:16 -0500

    final comments

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//esperamos a que el Dom se carge y luego ponemos el boton de submit en 
+//si no hay nada en el input de lo contrario permitimos que se use
 document.addEventListener("DOMContentLoaded", function () {
   document.querySelector("button").disabled = true;
   document.querySelector("input").onkeyup = () => {
@@ -8,9 +10,10 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   };
 });
+//iniciamos un nuevo array, en 0
 let tasks = [];
 console.log(tasks);
-
+//crear el div y le añades el texto que puso el usuario
 const division = document.getElementById("tasks");
 const createTaskDiv = (text) =>{
   const tarea = document.createElement("div");
@@ -22,9 +25,11 @@ const createTaskDiv = (text) =>{
       division.appendChild(tarea);
       
 } 
+//genera un Id nuevo Id para agregarlos el div y al boton de cierre
 const generateId = (text) =>{
     return text + Date.now()
 }
+//crear un boton "x" que se añade al div y va a tener la funcionalidad de eliminar la tarea
 const createXButton = (tarea) => {
   const removeButton = document.createElement("button");
   removeButton.id = generateId(tarea.value)
@@ -35,8 +40,9 @@ const createXButton = (tarea) => {
   })
   return removeButton
 }
+//tomar la informacion que esta en #input y agregarla a la variable tasks
 function addNewTask() {
-  //tomar la informacion que esta en #input y agregarla a la variable tasks
+  
   const text = document.querySelector("#input").value;
   document.querySelector("input").value = "";
   document.querySelector("button").disabled = true;
@@ -45,7 +51,7 @@ function addNewTask() {
     createTaskDiv(text);
     //quitarTask();
   };
-
+//busca el indice del elemento y luego con lo saca del array
 const eliminarTarea = (tareita) => {
     const item = tasks.indexOf(tareita.value)
    console.log(item)
@@ -55,7 +61,7 @@ const eliminarTarea = (tareita) => {
    parent.removeChild(child) 
    console.log(child)
 };
-
+//vacia el div padre y pone la longitud del array en 0
 const removeAll = () => {
 let parent = document.getElementById("tasks");
     parent.innerHTML = "";
